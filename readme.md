ba81ffcbf8c8d8cba7a4ba985d550bfa162a1865
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-08-04 01:25:52 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,55 @@
+# Fernet Keyring Tempfile
+
+> WARNING: you should read the source code and understand this package, or do
+> not use it. If you do choose to use it, then that is at your own risk. It is a
+> naive and simple secret storage system. The main purpose is to avoid storing
+> plaintext tokens on disk between python notebook sessions.
+>
+> A better solution would be to use the `keyring` package directly, but on
+> windows there is some
+> [silly limit on the size of a password (1280 characters?)](https://github.com/jaraco/keyring/issues/355)
+> that means I need to save my secret to disk instead.
+>
+> When used as demonstrated below it does not guard against any serious attack,
+> rather, it might stop a hypothetical file system scan from detecting an
+> unencrypted token stored on disk. If an attacker has remote code execution,
+> and can run a python script under your Username, then they can retrieve the
+> secret as easily as you can by running the code similar to the example below.
+> However if their process is running under another username, or they just got
+> access to your storage device without remote code execution, then perhaps this
+> method offers some level of protection.
+
+This package uses `cryptography.Fernet().encrypt()` to encrypt and store a file
+in your machines temporary folder (determined using `tempfile.gettempdir()`).
+The key is generated using `cryptography.Fernet.generate_key()` and is
+automatically stored in your system's secret storage using
+`keyring.set_password()`.
+
+```python
+from fernet_keyring_tempfile import FernetKeyringTempfile
+
+(
+    FernetKeyringTempfile(
+        application_name="TEST_APPLICATION_NAME"
+    )
+    .store("SECRET MESSAGE!".encode("utf-8"))
+)
+```
+
+Later:
+```python
+from fernet_keyring_tempfile import FernetKeyringTempfile
+
+print(
+    FernetKeyringTempfile(
+        application_name"TEST_APPLICATION_NAME"
+    )
+    .load()
+    .decode("utf-8")
+)
+# >> "SECRET MESSAGE!"
+```
+
+Note that if the passphrase (e.g. `"TEST_APPLICATION_NAME"`) appears in
+plaintext in the example above. This is a big security hole, since the attacker can
+presumably also see your source code.
---
+++ src/fernet_keyring_tempfile/__init__.py
@@ -0,0 +1,56 @@
+import keyring
+from keyring.errors import PasswordDeleteError, PasswordSetError
+import tempfile
+from pathlib import Path
+from dataclasses import dataclass
+from cryptography.fernet import Fernet
+
+@dataclass
+class FernetKeyringTempfile:
+    application_name:str
+    
+    def get_temp_file_path(self) -> Path:
+        return Path(tempfile.gettempdir()) / f"fkt_{self.application_name}"
+    
+
+    def get_keyring_service_name(self) -> str:
+        return f"fernet_keyring_tempfile.py_{self.application_name}"
+    
+
+    def get_or_create_key(self) -> bytes:
+        try:
+            key_existing = keyring.get_password(self.get_keyring_service_name(), "key")
+            if key_existing is not None:
+                return key_existing.encode("utf-8")
+            else:
+                key_fresh = Fernet.generate_key()
+                keyring.set_password(self.get_keyring_service_name(), "key", key_fresh.decode("utf-8"))
+                return key_fresh
+        except PasswordSetError as e:
+            raise ValueError("Unable to set password using keyring for some reason") from e
+
+
+    def store(self, data_plaintext:bytes) -> None:
+        with self.get_temp_file_path().open("wb") as temp_file:
+            key = self.get_or_create_key()
+            data_encrypted = Fernet(key).encrypt(data_plaintext)
+            temp_file.write(data_encrypted)
+    
+
+    def load(self) -> bytes:
+        with self.get_temp_file_path().open("rb") as temp_file:
+            key_existing = keyring.get_password(self.get_keyring_service_name(), "key")
+            if key_existing is None:
+                raise ValueError("No key found in keyring")
+            data_encrypted = temp_file.read()
+            data_plaintext = Fernet(key_existing.encode("utf-8")).decrypt(data_encrypted)
+            return data_plaintext
+    
+
+    def remove(self, missing_ok=True) -> None:
+        self.get_temp_file_path().unlink(missing_ok=missing_ok)
+        try:
+            keyring.delete_password(self.get_keyring_service_name(), "key")
+        except PasswordDeleteError as e:
+            if not missing_ok:
+                raise ValueError("Unable to delete password using keyring for some reason") from e
